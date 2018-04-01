55ab55c9428f3a53f400b752f4c83e2af9ab5869
Charlie <league@WTS-iMac-11.attlocal.net>
2018-04-01 12:51:58 -0700

    OldArrays

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -3,5 +3,32 @@ package _01_array_manipulation;
 
 
 public class Inserting {
+
+	public static int[] insertAt(int[] testArray, int i, int j) {
+		int[] aRay = new int[testArray.length + 1];
+		for (int j2 = 0; j2 < i; j2++) {
+			aRay[j2] = testArray[j2];
+		}
+		aRay[i] = j;
+		for (int j2 = 0; j2 < testArray.length - i; j2++) {
+			aRay[j2 + i + 1] = testArray[j2 + i];
+		}
+		return aRay;
+	}
+
+	public static String[] insertAlphabetically(String[] orderedArray, String string) {
+		String[] aRay = new String[orderedArray.length + 1];
+		int l = 0;
+			for (int i = 0; i < orderedArray.length + 1; i++) {
+				l++;
+				if (string.compareTo(orderedArray[l]) < 0){
+					aRay[i] = string;
+					l = l - 1;
+				}	else {
+					aRay[i] = orderedArray[l];
+				}
+			}
+		return aRay;
+	}
 	
 }
--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -2,4 +2,26 @@ package _01_array_manipulation;
 
 public class Sorting {
 
+	public static void sort(String[] testArray) {
+		
+		for (int e = 0; e < testArray.length - 1; e++) {
+		for (int i = 0; i < testArray.length - 1; i++) {
+			
+			if (testArray[i].compareTo(testArray[i+1]) > 0){
+				String temp = testArray[i +1];
+				testArray[i+ 1] = testArray[i];
+				testArray[i] = temp;
+				
+			}
+			
+			
+		}
+		
+	
+		}
+		
+		
+		
+	}
+
 }
